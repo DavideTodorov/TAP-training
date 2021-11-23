1d1dda1f28a9c086a1f160932b9229135174bde5
Davide Todorov <65759483+DavideTodorov@users.noreply.github.com>
2021-11-23 18:22:30 +0200

    Add jokes

--- CurrencyExchange/main.py
+++ CurrencyExchange/main.py
@@ -9,26 +9,37 @@ app = Flask(__name__, static_folder='templates/')
 cache = TTLCache(maxsize=100, ttl=10800)
 
 
+def get_joke():
+    joke_response = requests.get("https://v2.jokeapi.dev/joke/Any")
+    random_joke_setup = joke_response.json()["setup"]
+    random_joke_delivery = joke_response.json()["delivery"]
+    random_joke_text = random_joke_setup + " " + random_joke_delivery
+    return random_joke_text
+
+
 @cached(cache)
 @app.route('/', methods=['GET', 'POST'])
 def go_home():
     if request.method == 'GET':
-        return render_template('home.html')
+        random_joke_text = get_joke()
+
+        return render_template('home.html', joke=random_joke_text)
 
     if request.method == 'POST':
         first_currency = request.form['first_currency'].upper()
         second_currency = request.form['second_currency'].upper()
 
-        r = requests.get(f"https://open.er-api.com/v6/latest/{first_currency}")
+        currencies_response = requests.get(f"https://open.er-api.com/v6/latest/{first_currency}")
 
         try:
-            currency_dict = r.json()["rates"]
+            currency_dict = currencies_response.json()["rates"]
         except KeyError:
             currency_rate_str = f"'{first_currency}' is not a invalid currency!"
             return render_template('home.html', rate=currency_rate_str)
         except json.decoder.JSONDecodeError:
             currency_rate_str = f"'{first_currency}' is not a invalid currency!"
-            return render_template('home.html', rate=currency_rate_str)
+            random_joke_text = get_joke()
+            return render_template('home.html', rate=currency_rate_str, joke=random_joke_text)
 
         first_currency_val = currency_dict[first_currency]
 
@@ -36,13 +47,15 @@ def go_home():
             second_currency_val = currency_dict[second_currency]
         except KeyError:
             currency_rate_str = f"'{second_currency}' is not a invalid currency!"
-            return render_template('home.html', rate=currency_rate_str)
-
-        print()
+            random_joke_text = get_joke()
+            return render_template('home.html', rate=currency_rate_str, joke=random_joke_text)
 
         currency_rate_str = f"For 1 {first_currency} you can buy {second_currency_val / 1} {second_currency}"
 
-        return render_template('home.html', rate=currency_rate_str)
+        random_joke_text = get_joke()
+
+        return render_template('home.html', rate=currency_rate_str, joke=random_joke_text)
+
 
 
 if __name__ == '__main__':
