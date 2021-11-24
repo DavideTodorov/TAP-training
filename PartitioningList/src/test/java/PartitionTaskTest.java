c975e2ddfe01402a4ccb6e14f35c00755a886e10
Davide Todorov <65759483+DavideTodorov@users.noreply.github.com>
2021-11-24 17:29:30 +0200

    Update PartitionTaskTest.java

--- PartitioningList/src/test/java/PartitionTaskTest.java
+++ PartitioningList/src/test/java/PartitionTaskTest.java
@@ -24,4 +24,19 @@ public class PartitionTaskTest {
     public void Test4(){
         assertFalse(PartitionTask.findSubsequence(new int[]{1, 5, 3}));
     }
+
+    @Test
+    public void Test5(){
+        assertTrue(PartitionTask.findSubsequence(new int[]{7,1,3,5,9,10,1}));
+    }
+
+    @Test
+    public void Test6(){
+        assertTrue(PartitionTask.findSubsequence(new int[]{6,6,2,10}));
+    }
+
+    @Test
+    public void Test7(){
+        assertFalse(PartitionTask.findSubsequence(new int[]{6,6,1,10}));
+    }
 }
