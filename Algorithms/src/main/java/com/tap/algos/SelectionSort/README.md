51f0efbf94ff30dc852c46410a4385bc18eef6fb
Davide Todorov <65759483+DavideTodorov@users.noreply.github.com>
2021-11-25 16:49:50 +0200

    Created SelectionSort

--- Algorithms/src/main/java/com/tap/algos/SelectionSort/README.md
+++ Algorithms/src/main/java/com/tap/algos/SelectionSort/README.md
@@ -7,4 +7,13 @@
 
 ### Overview
 
+> A simple sorting algorithm with O(n^2) complexity.
+
 ### Algorithm explanation
+
+> Selection sort algorithm uses two enhanced for loops. From the outer one
+> we get the element on index 'i' which we consider to be the minimum element.
+> With the second for loop we iterate all the elements on the right of
+> index 'i' and find the true minimum element and after that we swap their
+> places. Everything on the left of index 'i' is considered sorted and
+> everything on the right is considered unsorted.
--- Algorithms/src/main/java/com/tap/algos/SelectionSort/SelectionSortMain.java
+++ Algorithms/src/main/java/com/tap/algos/SelectionSort/SelectionSortMain.java
@@ -1,4 +1,37 @@
 package com.tap.algos.SelectionSort;
 
+import com.tap.algos.Utils;
+
 public class SelectionSortMain {
+    public static void main(String[] args) {
+        int[] ints = Utils.generateArray(5);
+        SelectionSortMain.sort(ints);
+
+        for (int anInt : ints) {
+            System.out.println(anInt);
+        }
+    }
+
+    public static int[] sort(int[] arr){
+
+        for (int i = 0; i < arr.length; i++) {
+            int currMinimum = arr[i];
+            int currMinimumIndex = i;
+
+            for (int j = i+1; j < arr.length; j++) {
+                if (currMinimum > arr[j]){
+                    currMinimum = arr[j];
+                    currMinimumIndex = j;
+                }
+
+
+            }
+
+            arr[currMinimumIndex] = arr[i];
+            arr[i] = currMinimum;
+        }
+
+
+        return arr;
+    }
 }
