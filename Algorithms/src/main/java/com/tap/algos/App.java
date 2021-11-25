8e6cd78c6ecf56f0393613a358ca4b407e6d56e8
Davide Todorov <65759483+DavideTodorov@users.noreply.github.com>
2021-11-25 14:54:36 +0200

    Initial commit for Algorithms

---
+++ Algorithms/src/main/java/com/tap/algos/App.java
@@ -0,0 +1,13 @@
+package com.tap.algos;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.Random;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        org.openjdk.jmh.Main.main(args);
+
+    }
+}
---
+++ Algorithms/src/main/java/com/tap/algos/BubbleSort/BubbleSortMain.java
@@ -0,0 +1,12 @@
+package com.tap.algos.BubbleSort;
+
+public class BubbleSortMain {
+
+    public static int[] sort(int[] arr){
+        
+
+
+        return null;
+    }
+
+}
