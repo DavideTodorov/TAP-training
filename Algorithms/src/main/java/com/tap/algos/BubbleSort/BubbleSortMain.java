d38958beb01f25f255bcff311408f7e02024b0c9
Davide Todorov <65759483+DavideTodorov@users.noreply.github.com>
2021-11-25 15:15:10 +0200

    Created BubbleSort

--- Algorithms/src/main/java/com/tap/algos/BubbleSort/BubbleSortMain.java
+++ Algorithms/src/main/java/com/tap/algos/BubbleSort/BubbleSortMain.java
@@ -1,12 +1,32 @@
 package com.tap.algos.BubbleSort;
 
+import com.tap.algos.Utils;
+
 public class BubbleSortMain {
 
+    public static void main(String[] args) {
+        int[] ints = Utils.generateArray(10);
+        BubbleSortMain.sort(ints);
+
+        for (int anInt : ints) {
+            System.out.println(anInt);
+        }
+
+    }
+
     public static int[] sort(int[] arr){
-        
 
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[i] > arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
 
-        return null;
+        return arr;
     }
 
 }
--- Algorithms/src/main/java/com/tap/algos/BubbleSort/README.md
+++ Algorithms/src/main/java/com/tap/algos/BubbleSort/README.md
@@ -1,1 +1,19 @@
 # BubbleSort
+
+## Table of contents:
+
+* [Overview](#overview)
+* [Algorithm explanation](#algorithm-explanation)
+
+### Overview
+
+> Simple sorting algorithm with O(n^2) complexity.
+
+### Algorithm explanation:
+
+> BubbleSort uses two enhanced for loops to iterate over data.
+> With the 'i' in the first for loop we access the element with which we
+> will compare the rest of the elements. With 'j' from the inner for loop
+> we are accessing the element to be compared to. If the element at index 'i'
+> is greater than the element at index 'j' we swap their places. This process
+> will continue until we have traversed the array with the outer for loop.
