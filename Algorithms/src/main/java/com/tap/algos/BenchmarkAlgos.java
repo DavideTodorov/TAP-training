223b0be47dc7e477597ce10ba0cbcbf0741b3475
Davide Todorov <65759483+DavideTodorov@users.noreply.github.com>
2021-11-27 10:12:20 +0200

    Created benchmark tests

--- Algorithms/src/main/java/com/tap/algos/BenchmarkAlgos.java
+++ Algorithms/src/main/java/com/tap/algos/BenchmarkAlgos.java
@@ -1,5 +1,10 @@
 package com.tap.algos;
 
+import com.tap.algos.BubbleSort.BubbleSortMain;
+import com.tap.algos.InsertionSort.InsertionSortMain;
+import com.tap.algos.MergeSort.MergeSortMain;
+import com.tap.algos.Quicksort.QuicksortMain;
+import com.tap.algos.SelectionSort.SelectionSortMain;
 import org.openjdk.jmh.annotations.*;
 
 import java.util.concurrent.TimeUnit;
@@ -12,6 +17,7 @@ public class BenchmarkAlgos {
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
     @Fork(value = 0, warmups = 1)
     public void benchMarkInsertion() {
+        int[] ints = Utils.generateArray(10000);
     }
 
     @Benchmark
@@ -21,6 +27,8 @@ public class BenchmarkAlgos {
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
     @Fork(value = 0, warmups = 1)
     public void benchMarkQS() {
+        int[] ints = Utils.generateArray(10000);
+        QuicksortMain.sort(ints);
     }
 
     @Benchmark
@@ -30,6 +38,8 @@ public class BenchmarkAlgos {
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
     @Fork(value = 0, warmups = 1)
     public void benchMarkMS() {
+        int[] ints = Utils.generateArray(10000);
+        MergeSortMain.sort(ints);
     }
 
 
@@ -40,7 +50,8 @@ public class BenchmarkAlgos {
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
     @Fork(value = 1, warmups = 1)
     public void benchMarkBS() {
-
+        int[] ints = Utils.generateArray(10000);
+        BubbleSortMain.sort(ints);
     }
 
     @Benchmark
@@ -50,6 +61,8 @@ public class BenchmarkAlgos {
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
     @Fork(value = 0, warmups = 1)
     public void benchMarkIS() {
+        int[] ints = Utils.generateArray(10000);
+        InsertionSortMain.sort(ints);
     }
 
     @Benchmark
@@ -59,6 +72,8 @@ public class BenchmarkAlgos {
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
     @Fork(value = 0, warmups = 1)
     public void benchMarkSS() {
+        int[] ints = Utils.generateArray(10000);
+        SelectionSortMain.sort(ints);
     }
 
 }
