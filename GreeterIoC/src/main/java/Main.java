9200ed8db7f0f3ea1cc0103d05325090aa6baaff
Davide Todorov <65759483+DavideTodorov@users.noreply.github.com>
2021-12-01 14:05:34 +0200

    Refactor code

--- GreeterIoC/src/main/java/Main.java
+++ GreeterIoC/src/main/java/Main.java
@@ -1,12 +1,11 @@
-import IoCCointainer.Container;
 import greeter.Greeter;
 import greeter.readers.FileStringReader;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        Container container = new Container();
-        Greeter greeter = new Greeter(container.resolve(FileStringReader.class));
+        Greeter greeter = new Greeter(FileStringReader.class);
 
         //You can specify file path if you are using FileStringReader
         greeter.greet("src/main/resources/text.txt");
--- GreeterIoC/src/main/java/greeter/Greeter.java
+++ GreeterIoC/src/main/java/greeter/Greeter.java
@@ -1,16 +1,17 @@
 package greeter;
 
+import IoCCointainer.Container;
 import greeter.writers.ConsoleWriter;
 import greeter.readers.StringReader;
 import greeter.writers.Writer;
 
 public class Greeter {
-
+    private final Container container = new Container();
     private final Writer writer;
     private final StringReader stringReader;
 
-    public Greeter(StringReader stringReader) {
-        this.stringReader = stringReader;
+    public Greeter(Class<? extends StringReader> clazz) {
+        this.stringReader = container.resolve(clazz);
         this.writer = new ConsoleWriter();
     }
 
