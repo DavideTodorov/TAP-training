618210b55e4d3908abc940fdef65a101d96f4d24
Davide Todorov <65759483+DavideTodorov@users.noreply.github.com>
2021-12-14 11:19:19 +0200

    Add some custom mongo queries

--- SpringBoot/UserService/src/main/java/com/example/userservice/models/repositories/UserRepository.java
+++ SpringBoot/UserService/src/main/java/com/example/userservice/models/repositories/UserRepository.java
@@ -1,11 +1,13 @@
 package com.example.userservice.models.repositories;
 
+import com.example.userservice.models.entities.Transaction;
 import com.example.userservice.models.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -14,4 +16,11 @@ public interface UserRepository extends MongoRepository<User, UUID> {
     Optional<User> getUserByFirstName(String firstName);
 
     void deleteByFirstName(String firstName);
+
+
+    @Query("{'$expr':{$gte:[{$size:'$transactions'},?0]}}")
+    List<User> findAllWithTransactionsMoreThan(int count);
+
+    @Query("{firstName: {$regex: '?0[a-z]+'}}")
+    List<User> findAllWithNameStartingWithLetter(String letter);
 }
